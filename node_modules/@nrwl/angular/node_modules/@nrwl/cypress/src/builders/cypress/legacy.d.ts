653dea4d33f7a7b566c528fbdf6f391a6d616bed
Vamsi Krishna Saragadam <svamsi@pkrmsezdc.com>
2020-03-12 16:33:17 +0530

    removed node_moduels folder

--- node_modules/@nrwl/angular/node_modules/@nrwl/cypress/src/builders/cypress/legacy.d.ts
+++
@@ -1,4 +0,0 @@
-import { BuilderContext } from '@angular-devkit/architect';
-import { Observable } from 'rxjs';
-import { CypressBuilderOptions } from './cypress.impl';
-export declare function legacyCompile(options: CypressBuilderOptions, context: BuilderContext): Observable<string>;
--- node_modules/@nrwl/angular/src/schematics/storybook-configuration/configuration.d.ts
+++
@@ -1,3 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { StorybookConfigureSchema } from './schema';
-export default function (schema: StorybookConfigureSchema): Rule;
